860f90dc248d39bf170a6c3a17973746ea1f8485
Prithvi Jamadagni <52258420+Pritjam@users.noreply.github.com>
2021-06-22 20:50:22 -0500

    testing more stuff

--- project.py
+++ project.py
@@ -25,7 +25,7 @@ disp.begin()
 # Clear display.
 disp.clear()
 disp.display()
-disp.command(0xC8)
+
 
 
 # Create blank image for drawing.
@@ -58,25 +58,13 @@ font = ImageFont.load_default()
 
 while True:
 
+    disp.command(0xC8)
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell = True )
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True )
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
-
     # Write two lines of text.
 
-    draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
-    draw.text((x, top+8),     str(CPU), font=font, fill=255)
-    draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
-    draw.text((x, top+25),    str(Disk),  font=font, fill=255)
+    draw.text((x, top), "Testing Testing!")
 
     # Display image.
     disp.image(image)
